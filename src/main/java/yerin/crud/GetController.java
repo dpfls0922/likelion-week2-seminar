0a00c386402661ed2b985dc8e45bc9197b305fc1
dpfls0922 <dpfls0922@naver.com>
2024-07-23 20:29:07 +0900

    init: 초기 세팅

---
+++ src/main/java/yerin/crud/GetController.java
@@ -0,0 +1,15 @@
+package yerin.crud;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    // http://localhost:8080/api/v1/get-api/hello
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hello World";
+    }
+
+}
