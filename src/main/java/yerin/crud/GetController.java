bf0931e3aa8a5b5379baae787a53027034fbc33e
dpfls0922 <dpfls0922@naver.com>
2024-07-25 23:10:57 +0900

    add: pathvariable 방식 추가

--- src/main/java/yerin/crud/GetController.java
+++ src/main/java/yerin/crud/GetController.java
@@ -12,4 +12,21 @@ public class GetController {
         return "Hello World";
     }
 
+    // http://localhost:8080/api/v1/get-api/name
+    @GetMapping(value = "/name")
+    public String getName() {
+        return "Flature";
+    }
+
+    // http://localhost:8080/api/v1/get-api/variable1/{String 값}
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    // http://localhost:8080/api/v1/get-api/variable2/{String 값}
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var) {
+        return var;
+    }
 }
