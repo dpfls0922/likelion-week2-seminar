694cb9424a0e51258ae7d05e53eeeeed26a100a5
dpfls0922 <dpfls0922@naver.com>
2024-07-26 16:14:50 +0900

    add: Dto 객체 활용한 Get 메서드 구현

--- src/main/java/yerin/crud/Dto/MemberDto.java
+++ src/main/java/yerin/crud/Dto/MemberDto.java
@@ -1,2 +1,34 @@
-package yerin.crud.Dto;public class MemberDto {
+package yerin.crud.Dto;
+
+public class MemberDto {
+    private String name;
+    private String email;
+    private String organization;
+
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getOrganization() {
+        return organization;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+    @Override
+    public String toString() {
+        return "MemberDto{" +
+                "name=" + name + '\'' +
+                ", email=" + email + '\'' +
+                ", organization=" + organization + '\'' +
+                '}';
+    }
 }
