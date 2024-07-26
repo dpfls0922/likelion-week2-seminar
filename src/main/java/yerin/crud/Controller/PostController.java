03b143fc1fa6eaef2cf682a903a9441c95bb96d3
dpfls0922 <dpfls0922@naver.com>
2024-07-26 16:38:14 +0900

    add: PostController 추가

--- src/main/java/yerin/crud/Controller/PostController.java
+++ src/main/java/yerin/crud/Controller/PostController.java
@@ -1,2 +1,14 @@
-package yerin.crud.Controller;public class PostController {
+package yerin.crud.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExample() {
+        return "Hello Post API";
+    }
 }
