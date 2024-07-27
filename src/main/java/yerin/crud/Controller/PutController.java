eddda42849f58deb8078eef5aad570cd0828e92c
dpfls0922 <dpfls0922@naver.com>
2024-07-27 09:50:28 +0900

    add: PutController 추가

---
+++ src/main/java/yerin/crud/Controller/PutController.java
@@ -0,0 +1,10 @@
+package yerin.crud.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+}
